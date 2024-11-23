85b53901fb2b0a3f04c2bb037d5d48785d7790ac
Lee Ourand <lee@ourand.me>
2024-11-22 22:32:35 -0500

    Finish lexer for complete set of tokens Monkeylang supports

--- token/token.go
+++ token/token.go
@@ -14,6 +14,14 @@ const (
 	INT       = "INT"
 	ASSIGN    = "="
 	PLUS      = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
@@ -22,11 +30,21 @@ const (
 	RBRACE    = "}"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
+	IF       = "IF"
+	ELSE       = "ELSE"
+	TRUE       = "TRUE"
+	FALSE       = "FALSE"
+	RETURN       = "RETURN"
 )
 
 var keywords = map[string]TokenType{
 	"fn": FUNCTION,
 	"let": LET,
+	"if": IF,
+	"else": ELSE,
+	"true": TRUE,
+	"false": FALSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
